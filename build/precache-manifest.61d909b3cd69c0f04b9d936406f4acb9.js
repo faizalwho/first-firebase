55933fb6ccd485c5c5b852b70a9deaccc4437f39
faizalwho <49904350+faizalwho@users.noreply.github.com>
2021-02-11 21:46:16 +0530

    1

---
+++ build/precache-manifest.61d909b3cd69c0f04b9d936406f4acb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9b37e38e97e874dd5ff1894e451bf9",
+    "url": "/first-firebase/index.html"
+  },
+  {
+    "revision": "9d95118897cc1cd3579c",
+    "url": "/first-firebase/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "3686f08a4b001939af33",
+    "url": "/first-firebase/static/css/main.bcb5a572.chunk.css"
+  },
+  {
+    "revision": "9d95118897cc1cd3579c",
+    "url": "/first-firebase/static/js/2.6231a3a7.chunk.js"
+  },
+  {
+    "revision": "2520f30cb4e102284a8f5fa3a821a3bf",
+    "url": "/first-firebase/static/js/2.6231a3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3686f08a4b001939af33",
+    "url": "/first-firebase/static/js/main.591e1480.chunk.js"
+  },
+  {
+    "revision": "701592dc3d881fb7fcc9",
+    "url": "/first-firebase/static/js/runtime-main.a5341a45.js"
+  }
+]);
---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyATvZ3Qpz42dxj0_JPpmDAIhOsbdi34wPM",
+  authDomain: "myfirstpronjenkt.firebaseapp.com",
+  databaseURL: "https://myfirstpronjenkt-default-rtdb.firebaseio.com",
+  projectId: "myfirstpronjenkt",
+  storageBucket: "myfirstpronjenkt.appspot.com",
+  messagingSenderId: "113862623138",
+  appId: "1:113862623138:web:bc935ff1bcca21f9613cfb",
+  measurementId: "G-4153R8GPBT"
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
